96d98e0aafe79b9133723bb096604f8f0602f1f7
mike-clark-8192 <38510249+mike-clark-8192@users.noreply.github.com>
2023-12-08 00:40:01 -0700

    fixed typo

--- xecho/xecho.cpp
+++ xecho/xecho.cpp
@@ -3,13 +3,11 @@
 
 int _tmain(int argc, TCHAR* argv[])
 {
-    // Per https://learn.microsoft.com/en-us/windows/win32/api/processenv/nf-processenv-getcommandline<w|a>,
-    // GetCommandLine() has no failure mode, so no need to check for NULL.
     LPCTSTR commandLine = GetCommandLine();
     if (argc > 1 && 0 == _tcscmp(argv[1], _T("-c"))) {
         _ftprintf(stderr, _T("xecho argc=%d\n"), argc);
         for (int i = 0; i < argc; i++) {
-            _ftprintf(stderr, _T("excho argv[%d]=[%s]\n"), i, argv[i]);
+            _ftprintf(stderr, _T("xecho argv[%d]=[%s]\n"), i, argv[i]);
         }
     } else {
         _ftprintf(stderr, _T("xecho [%s]\n"), commandLine);
